d804988cfc7c1f05b281b33544906a23d0210abc
shenanigan11 <enzo2dessaint@gmail.com>
2026-02-22 19:39:44 +0100

    feat: #101 page login

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './pages/login/login.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginPage },
+  { path: '**', redirectTo: 'login' },
+];
