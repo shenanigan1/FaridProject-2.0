a33fb3d7c5485960c33f372402a8c5fa8bddbcfc
shenanigan11 <enzo2dessaint@gmail.com>
2026-02-27 15:41:04 +0100

    format

--- apps/backend/evaluations/models/__init__.py
+++ apps/backend/evaluations/models/__init__.py
@@ -1,6 +1,8 @@
 from .skill_answer import SkillAnswer as SkillAnswer
 from .evaluation_comment import EvaluationComment as EvaluationComment
-from .evaluation_section_assignment import EvaluationSectionAssignment as EvaluationSectionAssignment
+from .evaluation_section_assignment import (
+    EvaluationSectionAssignment as EvaluationSectionAssignment,
+)
 
 from .evaluation_question import EvaluationQuestion as EvaluationQuestion
 from .evaluation import Evaluation as Evaluation
