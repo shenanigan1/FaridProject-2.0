e25474e7d18ebe0e913ffa57e6b3e6afe8264b48
shenanigan11 <enzo2dessaint@gmail.com>
2026-02-27 15:38:39 +0100

    correction

--- apps/backend/evaluations/models/__init__.py
+++ apps/backend/evaluations/models/__init__.py
@@ -1,15 +1,14 @@
 from .skill_answer import SkillAnswer as SkillAnswer
 from .evaluation_comment import EvaluationComment as EvaluationComment
-from .evaluation_section_assignment import (
-    EvaluationSectionAssignment as EvaluationSectionAssignment,
-)
+from .evaluation_section_assignment import EvaluationSectionAssignment as EvaluationSectionAssignment
+
 from .evaluation_question import EvaluationQuestion as EvaluationQuestion
-from .evaluation import Evaluation as EvaluationStatus
+from .evaluation import Evaluation as Evaluation
 
 __all__ = [
     "SkillAnswer",
     "EvaluationComment",
     "EvaluationSectionAssignment",
     "EvaluationQuestion",
-    "EvaluationStatus",
+    "Evaluation",
 ]
