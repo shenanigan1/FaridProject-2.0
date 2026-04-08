5d69369b79898ac6a9627e459f7fb07f146eeccd
shenanigan1 <114864658+shenanigan1@users.noreply.github.com>
2026-04-08 21:41:18 +0200

    refactor(positions): declare explicit __all__ exports

--- apps/backend/positions/serializers/__init__.py
+++ apps/backend/positions/serializers/__init__.py
@@ -1,5 +1,4 @@
 from .position import PositionSerializer as PositionSerializer
 from .position import PublicPositionSerializer as PublicPositionSerializer
 
-__all__ = ["PositionSerializer"]
-__all__.append("PublicPositionSerializer")
+__all__ = ["PositionSerializer", "PublicPositionSerializer"]
